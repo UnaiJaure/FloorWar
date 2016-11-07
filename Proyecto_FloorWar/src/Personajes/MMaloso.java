13596ab5c671fe4c1cc045d8a13531b36ad6f002
UnaiJaure <Unai Jauregibeitia@opendeusto.es>
2016-11-07 17:21:15 +0100

    /**/ Intento de que funcione

--- Proyecto_FloorWar/src/Personajes/MMaloso.java
+++ Proyecto_FloorWar/src/Personajes/MMaloso.java
@@ -1,5 +1,7 @@
 package Personajes;
-
+/**Clase para instanciar elementos de Soldado de tipo MMaloso
+ * @author Unai Jauregibeitia y Miker Colmenares
+ */
 public class MMaloso extends Soldado{
 
 }
