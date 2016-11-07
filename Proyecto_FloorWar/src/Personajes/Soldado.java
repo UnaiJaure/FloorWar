aab734c825321c4e6ef66c72c4b17a8a0b034387
UnaiJaure <Unai Jauregibeitia@opendeusto.es>
2016-11-07 16:27:12 +0100

    Prueba de que va correctamente

--- Proyecto_FloorWar/src/Personajes/Soldado.java
+++ Proyecto_FloorWar/src/Personajes/Soldado.java
@@ -5,5 +5,29 @@ public class Soldado {
 	protected static String nombre;
 	protected static int hp;
 	
+	public Soldado(){}
+	
+	public Soldado(String nombre, int hp){
+		this.nombre = nombre;
+		this.hp = hp;
+	}
+
+	public static String getNombre() {
+		return nombre;
+	}
+
+	public static void setNombre(String nombre) {
+		Soldado.nombre = nombre;
+	}
+
+	public static int getHp() {
+		return hp;
+	}
+
+	public static void setHp(int hp) {
+		Soldado.hp = hp;
+	}
+	
+	
 	
 }
