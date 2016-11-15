53d69dae0323d6bfd3dcd6a8c57125ff019932a1
Txus <Txus@10.164.48.22>
2016-11-15 12:56:40 +0100

    francotirador construtor

--- Proyecto_FloorWar/src/Personajes/Francotirador.java
+++ Proyecto_FloorWar/src/Personajes/Francotirador.java
@@ -2,11 +2,15 @@ package Personajes;
 /**Clase para instanciar elementos de Soldado de tipo Franco-tirador
  * @author Unai Jauregibeitia y Miker Colmenares
  */
-public class Francotirador {
-	//el generalisimo
+public class Francotirador extends Soldado{
+	
+	public Francotirador(String nombre, int hp) {
+		super(nombre, hp);
+		// TODO Auto-generated constructor stub
+	}
 	
 	/** Constructor de Francotirador.
 	 *
 	 */
-
+	
 }
