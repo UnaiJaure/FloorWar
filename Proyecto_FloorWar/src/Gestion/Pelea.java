8a45937e99a2723559c9d689310ddba2d00f7ec1
Txus <Txus@10.164.52.121>
2016-11-18 14:00:53 +0100

    -x-Juego-x- -->Pelea

--- Proyecto_FloorWar/src/Gestion/Pelea.java
+++ Proyecto_FloorWar/src/Gestion/Pelea.java
@@ -1,5 +1,28 @@
 package Gestion;
 
+import Personajes.Francotirador;
+import Personajes.Ingeniero;
+import Personajes.MMaloso;
+import Personajes.Medico;
+
 public class Pelea {
-	
-}
+		Francotirador franco = new Francotirador("Stuart",100);
+		Ingeniero ing = new Ingeniero("Arturo",100,3);
+		Medico med = new Medico("Tomas",100);
+		MMaloso mm = new MMaloso("Fungecio",100);
+		
+		public Pelea() {
+			/*Francotirador franco = new Francotirador("Stuart",100);
+			Ingeniero ing = new Ingeniero("Arturo",100,3);
+			Medico med = new Medico("Tomas",100);
+			MMaloso mm = new MMaloso("Fungecio",100);*/
+		}
+		
+		public int ataque(){
+			return 0;
+			
+		}
+		
+		
+		
+	}
